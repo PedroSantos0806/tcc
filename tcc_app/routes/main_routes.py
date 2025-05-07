8a5a3458c6e86130c8eaab209422e144ff57ac7c
Pedro Santos <106214744+PedroSantos0806@users.noreply.github.com>
2025-05-07 20:59:29 -0300

    finalização do esqueleto do projeto 100% com code

--- tcc_app/routes/main_routes.py
+++ tcc_app/routes/main_routes.py
@@ -1,52 +1,35 @@
-# tcc_app/routes/main_routes.py
-from flask import Blueprint, render_template, redirect, url_for, request, session
-import plotly.express as px
-from functools import wraps
+from flask import Blueprint, render_template, redirect, url_for, session
+import plotly.graph_objs as go
+import plotly.io as pio
 
 main_bp = Blueprint('main', __name__)
-
-# Decorador para exigir login
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if 'usuario' not in session:
-            return redirect(url_for('auth.login'))
-        return f(*args, **kwargs)
-    return decorated_function
 
 @main_bp.route('/')
 def index():
     return render_template('index.html')
 
 @main_bp.route('/previsao')
-@login_required
 def previsao():
-    return render_template('previsao.html')
+    if 'usuario' not in session:
+        return redirect(url_for('auth.login'))
 
-@main_bp.route('/cadastrar_produto', methods=['GET', 'POST'])
-@login_required
-def cadastrar_produto():
-    if request.method == 'POST':
-        nome = request.form.get('nome')
-        print(f'Produto cadastrado: {nome}')
-        return redirect(url_for('main.index'))
-    return render_template('cadastrar_produto.html')
-
-@main_bp.route('/registrar_venda', methods=['GET', 'POST'])
-@login_required
-def registrar_venda():
-    if request.method == 'POST':
-        produto = request.form.get('produto')
-        quantidade = request.form.get('quantidade')
-        print(f'Venda registrada: {quantidade}x {produto}')
-        return redirect(url_for('main.index'))
-    return render_template('registrar_venda.html')
-
-@main_bp.route('/ver_previsao')
-@login_required
-def ver_previsao():
+    # Dados fictícios para o gráfico
     dias = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex']
-    vendas = [10, 15, 7, 12, 20]
-    grafico = px.line(x=dias, y=vendas, title='Previsão de Vendas (fictícia)', labels={'x': 'Dia', 'y': 'Quantidade'})
-    grafico_html = grafico.to_html(full_html=False)
-    return render_template('ver_previsao.html', grafico=grafico_html)
+    produto_a = [10, 12, 8, 14, 11]
+    produto_b = [5, 7, 6, 4, 8]
+
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=dias, y=produto_a, mode='lines+markers', name='Produto A'))
+    fig.add_trace(go.Scatter(x=dias, y=produto_b, mode='lines+markers', name='Produto B'))
+    fig.update_layout(title='Previsão de Vendas (Fictício)', xaxis_title='Dia', yaxis_title='Quantidade')
+
+    grafico_html = pio.to_html(fig, full_html=False)
+
+    return render_template('previsao.html', grafico=grafico_html)
+
+@main_bp.route('/registrar-venda')
+def registrar_venda():
+    if 'usuario' not in session:
+        return redirect(url_for('auth.login'))
+
+    return render_template('registrar_venda.html')
