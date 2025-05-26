4a7633747c6a5cb01f595543dbe6ecbf83e92028
Pedro Santos <106214744+PedroSantos0806@users.noreply.github.com>
2025-05-26 19:59:27 -0300

    mudanças na métricas de acesso ao banco de dados

--- tcc_app/db.py
+++ tcc_app/db.py
@@ -4,16 +4,12 @@ from flask import current_app, g
 
 def get_db_connection():
     if 'db_conn' not in g:
-        # Caminho para o certificado SSL
-        ssl_ca_path = os.path.join(os.getcwd(), 'tcc_app', 'certs', 'DigiCertGlobalRootCA.pem')
-
         config = {
-            'user': os.environ.get('DB_USER'),
+            'user': os.environ.get('DB_USER') + '@servidortcc',  # concatena domínio do servidor
             'password': os.environ.get('DB_PASSWORD'),
             'host': os.environ.get('DB_HOST'),
             'database': os.environ.get('DB_NAME'),
-            'port': int(os.environ.get('DB_PORT', 3306)),
-            'ssl_ca': ssl_ca_path
+            'port': int(os.environ.get('DB_PORT', 3306))
         }
 
         g.db_conn = mysql.connector.connect(**config)
