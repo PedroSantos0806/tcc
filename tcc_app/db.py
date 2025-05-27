e1b3835d2eaaafc684314388f4b1099420f4cd01
Pedro Santos <106214744+PedroSantos0806@users.noreply.github.com>
2025-05-27 09:17:08 -0300

    tentando resolver problema de conexão com o banco

--- tcc_app/db.py
+++ tcc_app/db.py
@@ -4,12 +4,15 @@ from flask import current_app, g
 
 def get_db_connection():
     if 'db_conn' not in g:
+        ssl_ca_path = os.path.join(os.getcwd(), 'tcc_app', 'certs', 'DigiCertGlobalRootCA.pem')
+
         config = {
-            'user': os.environ.get('DB_USER') + '@servidortcc',  # concatena domínio do servidor
+            'user': os.environ.get('DB_USER') + '@' + os.environ.get('DB_SERVER'),
             'password': os.environ.get('DB_PASSWORD'),
             'host': os.environ.get('DB_HOST'),
             'database': os.environ.get('DB_NAME'),
-            'port': int(os.environ.get('DB_PORT', 3306))
+            'port': int(os.environ.get('DB_PORT', 3306)),
+            'ssl_ca': ssl_ca_path
         }
 
         g.db_conn = mysql.connector.connect(**config)
