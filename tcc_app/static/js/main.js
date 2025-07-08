a16f73140d28812acb43b22c45f8fc36a5f81cde
Pedro Santos <106214744+PedroSantos0806@users.noreply.github.com>
2025-07-08 16:44:36 -0300

    atualizações no layout e adição de novos recursos

--- tcc_app/static/js/main.js
+++ tcc_app/static/js/main.js
@@ -1,6 +1,16 @@
-// static/script.js
-
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Script carregado com sucesso!");
-  });
-  
+    const toggleBtn = document.getElementById("toggleSenha");
+    const senhaInput = document.getElementById("senha");
+
+    if (toggleBtn && senhaInput) {
+        toggleBtn.addEventListener("click", () => {
+            if (senhaInput.type === "password") {
+                senhaInput.type = "text";
+                toggleBtn.textContent = "🙈";
+            } else {
+                senhaInput.type = "password";
+                toggleBtn.textContent = "👁️";
+            }
+        });
+    }
+});
